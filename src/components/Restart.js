f8f01ca856f05b79a6f481344f2dd234edafde7f
AbarThMarcin <marcin.wesolowski89@gmail.com>
2022-03-21 15:24:13 +0100

    Final

--- src/components/Restart.js
+++ src/components/Restart.js
@@ -2,16 +2,15 @@ import { useContext } from 'react'
 import { getCurrentBoardInfo } from '../func/boardFunctions'
 import { CellContext } from '../App'
 
-
 const Restart = ({ timer }) => {
    const { boardEl, restart } = useContext(CellContext)
-   const onClick = () => {
+   const handleClick = () => {
       restart(getCurrentBoardInfo())
       boardEl.current.classList.remove('unclickable')
       timer.resetTimer()
    }
 
-   return <button className="btnRestart" onClick={() => onClick()}></button>
+   return <button className="btnRestart" onClick={handleClick}></button>
 }
 
 export default Restart
